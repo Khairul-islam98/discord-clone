722d77226e3ea5fdb7f7900fed98a8a8180439d9
khairul-islam98 <janeyrahman900@gmail.com>
2024-11-01 05:25:11 +0600

    create leave modal

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,6 +5,7 @@ import { InviteModal } from "../modals/invite-modal";
 import { EditServerModal } from "../modals/edit-server-modal";
 import { MembersModal } from "../modals/members-modal";
 import { CreateChannelModal } from "../modals/create-channel-modal";
+import { LeaveModal } from "../modals/leave-modal";
 
 
 
@@ -27,6 +28,7 @@ export const ModalProvider = () => {
             <EditServerModal />
             <MembersModal />
             <CreateChannelModal />
+            <LeaveModal />
         </>
     );
 };
--- components/server/server-hearder.tsx
+++ components/server/server-hearder.tsx
@@ -83,7 +83,9 @@ export const ServerHearder = ({ server, role }: ServerHearderProps) => {
           </DropdownMenuItem>
         )}
         {!isAdmin && (
-          <DropdownMenuItem className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem 
+            onClick={() => onOpen("leaveServer", { server})}
+          className="text-rose-500 px-3 py-2 text-sm cursor-pointer">
             Leave Server
             <LogOut className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
