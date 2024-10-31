ba402213adbb7169ada8d341933b010dee63c271
khairul-islam98 <janeyrahman900@gmail.com>
2024-11-01 05:40:52 +0600

    create delete server

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -6,6 +6,7 @@ import { EditServerModal } from "../modals/edit-server-modal";
 import { MembersModal } from "../modals/members-modal";
 import { CreateChannelModal } from "../modals/create-channel-modal";
 import { LeaveModal } from "../modals/leave-modal";
+import { DeleteModal } from "../modals/delete-modal";
 
 
 
@@ -29,6 +30,7 @@ export const ModalProvider = () => {
             <MembersModal />
             <CreateChannelModal />
             <LeaveModal />
+            <DeleteModal />
         </>
     );
 };
