6e09b360784af7c3936babbc2622ad3dc56541cc
khairul-islam98 <janeyrahman900@gmail.com>
2024-11-01 06:10:05 +0600

    added  serverSearch and all icon and channel

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,56 +1,121 @@
 import { currentProfle } from "@/lib/current-profile";
 import { db } from "@/lib/db";
-import { ChannelType } from "@prisma/client";
+import { ChannelType, MemberRole } from "@prisma/client";
 import { redirect } from "next/navigation";
 import { ServerHearder } from "./server-hearder";
+import { ScrollArea } from "../ui/scroll-area";
+import { ServerSearch } from "./server-search";
+import { Hash, Mic, ShieldAlert, ShieldCheck, VideoIcon } from "lucide-react";
 
 interface serverSidebarProps {
-    serverId: string;
+  serverId: string;
 }
 
-export const ServerSidebar = async({serverId}: serverSidebarProps) => {
-    const profile = await currentProfle();
-     if(!profile){
-        return redirect("/")
-     }
-     const server = await db.server.findUnique({
-         where: {
-            id: serverId,
+const iconMap = {
+    [ChannelType.TEXT]: <Hash className="w-4 h-4 mr-2" />,
+    [ChannelType.AUDIO]: <Mic className="w-4 h-4 mr-2" />,
+    [ChannelType.VIDEO]: <VideoIcon className="w-4 h-4 mr-2" />
+}
+const roleIconMap = {
+    [MemberRole.GUEST]: null,
+    [MemberRole.MODERATOR]: <ShieldCheck className="w-4 h-4 mr-2 text-indigo-500" />,
+    [MemberRole.ADMIN]: <ShieldAlert className="w-4 h-4 mr-2 text-rose-500" />,
+}
 
-         },
-         include: {
-            channels: {
-                orderBy: {
-                    createdAt: "asc"
+export const ServerSidebar = async ({ serverId }: serverSidebarProps) => {
+  const profile = await currentProfle();
+  if (!profile) {
+    return redirect("/");
+  }
+  const server = await db.server.findUnique({
+    where: {
+      id: serverId,
+    },
+    include: {
+      channels: {
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+      members: {
+        include: {
+          profile: true,
+        },
+        orderBy: {
+          role: "asc",
+        },
+      },
+    },
+  });
+  const textChannels = server?.channels.filter(
+    (channel) => channel.type === ChannelType.TEXT
+  );
+  const audioChannels = server?.channels.filter(
+    (channel) => channel.type === ChannelType.AUDIO
+  );
+  const videoChannels = server?.channels.filter(
+    (channel) => channel.type === ChannelType.VIDEO
+  );
+
+  const members = server?.members.filter(
+    (member) => member.profileId !== profile?.id
+  );
+
+  if (!server) {
+    return redirect("/");
+  }
+  const role = server?.members.find(
+    (member) => member.profileId === profile?.id
+  )?.role;
+  return (
+    <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
+      <ServerHearder server={server} role={role} />
+      <ScrollArea
+      className="flex-1 px-3"
+      >
+        <div className="mt-2">
+            <ServerSearch 
+            data={[
+                {
+                    label: "Text Channels",
+                    type: "channel",
+                    data: textChannels?.map(channel => ({
+                        icon: iconMap[channel.type],
+                        name: channel.name,
+                        id: channel.id
+                    }))
+                },
+                {
+                    label: "Voice Channels",
+                    type: "channel",
+                    data: audioChannels?.map(channel => ({
+                        icon: iconMap[channel.type],
+                        name: channel.name,
+                        id: channel.id
+                    }))
+                },
+                {
+                    label: "Video Channels",
+                    type: "channel",
+                    data: videoChannels?.map(channel => ({
+                        icon: iconMap[channel.type],
+                        name: channel.name,
+                        id: channel.id
+                    }))
+                },
+                {
+                    label: "Members",
+                    type: "member",
+                    data: members?.map(member => ({
+                        icon: roleIconMap[member.role],
+                        name: member.profile.name,
+                        id: member.id
+                    }))
                 }
-            },
-            members: {
-                 include:{
-                    profile:  true,
-                 },
-                 orderBy: {
-                    role: "asc"
-                 }
-            }
-         }
-     })
-     const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT);
-     const audioChannels = server?.channels.filter((channel) => channel.type === ChannelType.AUDIO);
-     const videoChannels = server?.channels.filter((channel) => channel.type === ChannelType.VIDEO);
-
-     const members = server?.members.filter((member) => member.profileId !== profile?.id);
-
-     if(!server){
-         return redirect("/")
-     } 
-     const role = server?.members.find((member) => member.profileId === profile?.id)?.role;
-    return (
-        <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
-            <ServerHearder
-                server={server}
-                role={role}
+            ]}
             />
         </div>
-    );
+      </ScrollArea>
+    </div>
+  );
 };
-
