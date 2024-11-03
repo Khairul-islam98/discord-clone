1974315dd494d8676662a5764b49c5e3343b148d
khairul-islam98 <janeyrahman900@gmail.com>
2024-11-03 06:30:14 +0600

    add socket provider for real-time communication setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "../components/providers/theme-provider";
 import { cn } from "../lib/utils";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -34,8 +35,10 @@ export default function RootLayout({
             enableSystem
             storageKey="discord-theme"
           >
+            <SocketProvider>
             <ModalProvider />
             {children}
+            </SocketProvider>
           </ThemeProvider>
         </body>
       </html>
