917292a64e6dd24074a6c8ec93e95ebe30432e48
khairul-islam98 <janeyrahman900@gmail.com>
2024-11-03 13:32:09 +0600

    implement message handling API with profile validation and error responses

--- lib/current-profile-pages.ts
+++ lib/current-profile-pages.ts
@@ -3,7 +3,7 @@ import { getAuth } from "@clerk/nextjs/server";
 import { db } from "./db";
 import { NextApiRequest } from "next";
 
-export const currentProfle = async (req: NextApiRequest) => {
+export const currentProflePage = async (req: NextApiRequest) => {
   const { userId } = getAuth(req);
   if (!userId) {
     return null;
