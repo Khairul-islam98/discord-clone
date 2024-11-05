f73e9770f8381f1c4bd4b62ed5279f24a1c8abb2
khairul-islam98 <janeyrahman900@gmail.com>
2024-11-05 07:29:04 +0600

    refactor: simplify FileUpload component by updating file handling and state management

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,36 +1,29 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 import { UploadDropzone } from "@/lib/uploadthing";
 
 interface FileUploadProps {
-  onChange: (file?: { url: string; type: string } | string) => void;
-  value: string | { url: string; type: string };
+  onChange: (url?: string) => void;
+  value: string;
   endpoint: "messageFile" | "serverImage";
 }
 
 export function FileUpload({
   onChange,
   value,
-  endpoint,
+  endpoint
 }: FileUploadProps) {
-
-   
-
-    const url = typeof value === "object" ? value.url : "";
-    const type = typeof value === "object" ? value.type?.split("/").pop() : "";
-    
+  const [fileType, setFileType] = useState("");
 
 
-
-
-  if (url && type !== "pdf") {
+  if (value && fileType !== "application/pdf")  {
     return (
       <div className="relative h-20 w-20">
-        <Image fill src={url} alt="Upload" className="rounded-full" />
+        <Image fill src={value} alt="Upload" className="rounded-full" />
         <button
           onClick={() => onChange("")}
           className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
@@ -42,17 +35,17 @@ export function FileUpload({
     );
   }
 
-  if (type === "pdf") {
+  if (value && fileType === "application/pdf")  {
     return (
       <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
         <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
         <a
-          href={url}
+          href={value}
           target="_blank"
           rel="noopener noreferrer"
           className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline truncate w-52"
         >
-          {url}
+          {value}
         </a>
         <button
           onClick={() => onChange("")}
@@ -69,13 +62,8 @@ export function FileUpload({
     <UploadDropzone
       endpoint={endpoint}
       onClientUploadComplete={(res) => {
-
-        if(res?.[0]) {
-            const { url, type } = res[0];
-            onChange({url, type});
-        }
-        
-       
+        setFileType(res?.[0].type);
+        onChange(res?.[0].url);
       }}
       onUploadError={(error: Error) => console.error(error.message)}
     />
